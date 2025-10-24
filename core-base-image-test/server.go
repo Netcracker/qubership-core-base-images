2e576d828f492e01ee6a7830fbc9047c6b4092be
Ksiona <kseniia.shmoilova.qubership@gmail.com>
2025-10-24 18:14:14 +0400

    feat: test base images with test applications

--- core-base-image-test/server.go
+++ core-base-image-test/server.go
@@ -14,15 +14,16 @@ func main() {
     defer func() {
         if r := recover(); r != nil {
             fmt.Printf("Recovered from test-service launch panic: %v\n", r)
+			os.Exit(1)
         }
     }()
 
     fmt.Println("Handle /call_from_service request")
     resp, err := http.Get(testContainerUrl)
-    if err != nil {
+    if err != nil && resp.StatusCode !=200 {
         fmt.Printf("Failed to call service: %v\n", err)
-        return
-    }
+        os.Exit(1)
+    } 
     defer resp.Body.Close()
 
     fmt.Printf("Responded with: %d\n", resp.StatusCode)
