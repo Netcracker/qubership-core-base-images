b71c7b0afe41fc48248ffbd4952a2d4d99e6a25d
Ksiona <kseniia.shmoilova.qubership@gmail.com>
2025-12-12 18:20:17 +0400

    feat: split and validated certificates before update

--- base-image-test-service/test_certificates_processing.sh
+++ base-image-test-service/test_certificates_processing.sh
@@ -13,14 +13,14 @@ echo Validate certificate load to java keystore
 docker run -v ${TEST_DIR}:/tmp/cert/ -e CERTIFICATE_FILE_PASSWORD=testit --rm ${JAVA_BASE_IMAGE} \
        keytool -v -list -keystore /etc/ssl/certs/java/cacerts -storepass testit -alias testcert.pem 
 
-echo Validate ca certificates were copied to store and splitted
-docker run -v ${TEST_DIR}:/tmp/cert/ --rm ${CORE_BASE_IMAGE} \
-       cat /etc/ssl/certs/ca-certificates.crt | awk -v decoder='openssl x509 -noout -subject -enddate 2>/dev/null' '/BEGIN/{close(decoder)};{print | decoder}' | grep -i -E "testcerts.com|valid1|valid2"
-
 echo Validate kubernetes ca certificate load to image trust store
 docker run -v ${TEST_DIR}/test-k8s-ca.crt:/var/run/secrets/kubernetes.io/serviceaccount/ca.crt --rm ${CORE_BASE_IMAGE} \
        cat /etc/ssl/certs/ca-certificates.crt | awk -v decoder='openssl x509 -noout -subject -enddate 2>/dev/null' '/BEGIN/{close(decoder)};{print | decoder}' | grep testcerts.com
 docker run -v ${TEST_DIR}/test-k8s-ca.crt:/var/run/secrets/kubernetes.io/serviceaccount/ca.crt --rm ${JAVA_BASE_IMAGE} \
        keytool -cacerts -storepass changeit -v -list | grep testcerts.com
+
+echo Validate ca certificates were copied to store and splitted
+docker run -v ${TEST_DIR}:/tmp/cert/ --rm ${CORE_BASE_IMAGE} \
+      cat /etc/ssl/certs/ca-certificates.crt | awk -v decoder='openssl x509 -noout -subject -enddate 2>/dev/null' '/BEGIN/{close(decoder)};{print | decoder}' | grep -i -E "testcerts.com|valid1|valid2"
 
 echo "Test passed"
