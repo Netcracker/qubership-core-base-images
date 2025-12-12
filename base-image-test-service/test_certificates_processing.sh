0a02063a98831f328ff64e8e66b4f0855106ec98
Ksiona <kseniia.shmoilova.qubership@gmail.com>
2025-12-12 14:29:18 +0400

    feat: split and validated certificates before update

---
+++ base-image-test-service/test_certificates_processing.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -x
+
+image_tag=$1
+
+JAVA_BASE_IMAGE=ghcr.io/netcracker/qubership-java-base:${image_tag}
+CORE_BASE_IMAGE=ghcr.io/netcracker/qubership-core-base:${image_tag}
+
+TEST_DIR="./certs"
+
+echo Validate certificate load to java keystore
+docker run -v ${TEST_DIR}:/tmp/cert/ -e CERTIFICATE_FILE_PASSWORD=testit --rm ${JAVA_BASE_IMAGE} \
+       keytool -v -list -keystore /etc/ssl/certs/java/cacerts -storepass testit -alias testcert.pem || exit 1
+
+echo Validate ca certificates were copied to store
+docker run -v ${TEST_DIR}:/tmp/cert/ --rm ${CORE_BASE_IMAGE} \
+       cat /etc/ssl/certs/ca-certificates.crt | awk -v decoder='openssl x509 -noout -subject -enddate 2>/dev/null' '/BEGIN/{close(decoder)};{print | decoder}' | grep testcerts.com || exit 1
+
+echo Validate kubernetes ca certificate load to image trust store
+docker run -v ${TEST_DIR}/test-k8s-ca.crt:/var/run/secrets/kubernetes.io/serviceaccount/ca.crt --rm ${CORE_BASE_IMAGE} \
+       cat /etc/ssl/certs/ca-certificates.crt | awk -v decoder='openssl x509 -noout -subject -enddate 2>/dev/null' '/BEGIN/{close(decoder)};{print | decoder}' | grep testcerts.com || exit 1
+docker run -v ${TEST_DIR}/test-k8s-ca.crt:/var/run/secrets/kubernetes.io/serviceaccount/ca.crt --rm ${JAVA_BASE_IMAGE} \
+       keytool -cacerts -storepass changeit -v -list | grep testcerts.com || exit 1
+
+echo Test ca certificates were splitted
+rm -rf test.log
+docker run -v ${TEST_DIR}:/tmp/cert/ --rm ghcr.io/netcracker/qubership-core-base:${image_tag} | tee test.log
+
+if [[ $(grep -c "does not contain exactly one certificate or CRL" test.log) -gt 0 ]]; then
+  echo "Some certificates were not splitted"
+  exit 1
+else
+  echo "Test passed"
+fi
