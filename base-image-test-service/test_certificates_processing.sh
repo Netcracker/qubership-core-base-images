ba2850d14b4db7d7d4b9e61837af9e9a017ecb37
Sergey Lisovoy <lis0x90@gmail.com>
2025-12-13 18:11:41 +0500

    fix: tests

--- base-image-test-service/test_certificates_processing.sh
+++ base-image-test-service/test_certificates_processing.sh
@@ -10,20 +10,20 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 TEST_DIR="$SCRIPT_DIR/certs"
 
 echo Validate certificate load to java keystore
-docker run -v ${TEST_DIR}:/tmp/cert/ -e CERTIFICATE_FILE_PASSWORD=testit --rm ${JAVA_BASE_IMAGE} \
-       keytool -v -list -keystore /etc/ssl/certs/java/cacerts -storepass testit -alias testcert.pem 
+docker run -v "${TEST_DIR}":/tmp/cert/ -e CERTIFICATE_FILE_PASSWORD=testit --rm "${JAVA_BASE_IMAGE}" \
+       keytool -v -list -cacerts -storepass testit -alias testcert_000.pem
 
 echo Validate kubernetes ca certificate load to image trust store
-docker run -v ${TEST_DIR}/test-k8s-ca.crt:/var/run/secrets/kubernetes.io/serviceaccount/ca.crt --rm ${CORE_BASE_IMAGE} \
+docker run -v "${TEST_DIR}"/test-k8s-ca.crt:/var/run/secrets/kubernetes.io/serviceaccount/ca.crt --rm "${CORE_BASE_IMAGE}" \
        cat /etc/ssl/certs/ca-certificates.crt | awk -v decoder='openssl x509 -noout -subject -enddate 2>/dev/null' '/BEGIN/{close(decoder)};{print | decoder}' | grep testcerts.com
-docker run -v ${TEST_DIR}/test-k8s-ca.crt:/var/run/secrets/kubernetes.io/serviceaccount/ca.crt --rm ${JAVA_BASE_IMAGE} \
+docker run -v "${TEST_DIR}"/test-k8s-ca.crt:/var/run/secrets/kubernetes.io/serviceaccount/ca.crt --rm "${JAVA_BASE_IMAGE}" \
        keytool -cacerts -storepass changeit -v -list | grep testcerts.com
 
 echo Validate ca certificates were copied to store and splitted
-docker run -v ${TEST_DIR}:/tmp/cert/ --rm ${CORE_BASE_IMAGE} \
+docker run -v "${TEST_DIR}":/tmp/cert/ --rm "${CORE_BASE_IMAGE}" \
       cat /etc/ssl/certs/ca-certificates.crt > processed.crt
 for cn in 'testcerts.com' 'valid1' 'valid2'; do
-  cat processed.crt | awk -v decoder='openssl x509 -noout -subject -enddate 2>/dev/null' '/BEGIN/{close(decoder)};{print | decoder}' | grep $cn; 
+  <processed.crt awk -v decoder='openssl x509 -noout -subject -enddate 2>/dev/null' '/BEGIN/{close(decoder)};{print | decoder}' | grep $cn;
 done      
 
 echo "Test passed"
