13d92a65ea94a631d4766e0b00dc6f1c06ef599d
Sergey Lisovoy <lis0x90@gmail.com>
2025-12-14 22:47:05 +0500

    fix: update tests

--- base-image-test-service/test_certificates_processing.sh
+++ base-image-test-service/test_certificates_processing.sh
@@ -1,29 +1,56 @@
 #!/usr/bin/env bash
 set -ex
 
-image_tag=$1
-
-JAVA_BASE_IMAGE=ghcr.io/netcracker/qubership-java-base:${image_tag}
-CORE_BASE_IMAGE=ghcr.io/netcracker/qubership-core-base:${image_tag}
-
+CORE_BASE_IMAGE=ghcr.io/netcracker/qubership-core-base:$1
+JAVA_BASE_IMAGE=ghcr.io/netcracker/qubership-java-base:$1
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 TEST_DIR="$SCRIPT_DIR/certs"
 
-echo Validate certificate load to java keystore
-docker run -v "${TEST_DIR}":/tmp/cert/ -e CERTIFICATE_FILE_PASSWORD=testit --rm "${JAVA_BASE_IMAGE}" \
-       keytool -v -list -cacerts -storepass testit -alias testcert_000.pem
+fail() {
+  echo "Test error: $1" >&2
+  false
+}
 
-echo Validate kubernetes ca certificate load to image trust store
-docker run -v "${TEST_DIR}"/test-k8s-ca.crt:/var/run/secrets/kubernetes.io/serviceaccount/ca.crt --rm "${CORE_BASE_IMAGE}" \
-       cat /etc/ssl/certs/ca-certificates.crt | awk -v decoder='openssl x509 -noout -subject -enddate 2>/dev/null' '/BEGIN/{close(decoder)};{print | decoder}' | grep testcerts.com
-docker run -v "${TEST_DIR}"/test-k8s-ca.crt:/var/run/secrets/kubernetes.io/serviceaccount/ca.crt --rm "${JAVA_BASE_IMAGE}" \
-       keytool -cacerts -storepass changeit -v -list | grep testcerts.com
+export_image_trust_store() {
+  local image=$1
+  local output_file=$2
+  echo "Export certificate list from: $image"
+  docker run --rm -it \
+      -v "${TEST_DIR}":/tmp/cert/ \
+      -v "$(pwd)":/out/ \
+      "${image}" \
+      cp /etc/ssl/certs/ca-certificates.crt "/out/${output_file}.crt"
 
-echo Validate ca certificates were copied to store and splitted
-docker run -v "${TEST_DIR}":/tmp/cert/ --rm "${CORE_BASE_IMAGE}" \
-      cat /etc/ssl/certs/ca-certificates.crt > processed.crt
-for cn in 'testcerts.com' 'valid1' 'valid2'; do
-  <processed.crt awk -v decoder='openssl x509 -noout -subject -enddate 2>/dev/null' '/BEGIN/{close(decoder)};{print | decoder}' | grep $cn;
-done      
+  # extract certificates list from certs file
+  openssl crl2pkcs7 -nocrl -certfile "${output_file}.crt" | openssl pkcs7 -print_certs -noout >"${output_file}"
+}
 
-echo "Test passed"
+export_java_keystore() {
+  local image=$1
+  local output_file=$2
+  echo "Export certificate list from: $image"
+  docker run --rm -it \
+      -v "${TEST_DIR}":/tmp/cert/ \
+      -v "$(pwd)":/out/ \
+      -eCERTIFICATE_FILE_PASSWORD=abc12345 \
+      "${image}" \
+      keytool -list -cacerts -storepass abc12345 -v >${output_file}
+}
+
+assert_tests() {
+  local output_file=$1
+  <"$output_file" grep -e "CN\s*=\s*qubership-test" >/dev/null || fail "cert from testcert.pem is missed"
+  <"$output_file" grep -e "CN\s*=\s*valid2" >/dev/null || fail "cert from multi_cert.crt is missed"
+  <"$output_file" grep -e "CN\s*=\s*valid1" >/dev/null || fail "cert from multi_cert.crt is missed"
+  <"$output_file" grep -e "CN\s*=\s*testcerts.com" >/dev/null || fail "cert from test-k8s-ca.crt is missed"
+}
+
+export_image_trust_store "$CORE_BASE_IMAGE" exported-certs.list
+assert_tests exported-certs.list
+
+export_image_trust_store "$JAVA_BASE_IMAGE" exported-certs.list
+assert_tests exported-certs.list
+export_java_keystore "$JAVA_BASE_IMAGE" exported-certs.list
+assert_tests exported-certs.list
+
+echo "All tests passed"
