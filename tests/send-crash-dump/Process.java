17d7ba6298a30aec2f0f76b23e2c1638f54de2b9
Sergey Lisovoy <lis0x90@gmail.com>
2026-01-09 23:37:14 +0500

    chore: add test for send crash dump  (#120)
    * chore: add test
    
    * chore: add test
    
    * chore: add test

---
+++ tests/send-crash-dump/Process.java
@@ -0,0 +1,6 @@
+public class Process {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Started java test process");
+        Thread.sleep(100000);
+    }
+}
---
+++ tests/send-crash-dump/diag/diag-bootstrap.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+MARKER_MESSAGE=">>>>>> Captured send_crash_dump execution"
+
+# This is fake substitution of diagtools to capture calling of `send_crash_dump` function
+send_crash_dump() {
+  echo "$MARKER_MESSAGE"
+}
+export send_crash_dump
+
