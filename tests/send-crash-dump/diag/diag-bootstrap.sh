b0a9154fef241975146a2366f8e25e75ede1ab9c
Ksiona <kseniia.shmoilova.qubership@gmail.com>
2026-01-28 13:20:16 +0400

    chore: fix back compatibility

--- tests/send-crash-dump/diag/diag-bootstrap.sh
+++ tests/send-crash-dump/diag/diag-bootstrap.sh
@@ -3,10 +3,7 @@
 MARKER_MESSAGE=">>>>>> Captured send_crash_dump execution"
 
 # This is fake substitution of diagtools to capture calling of `send_crash_dump` function
-send_crash_dump() {
-  local fs_mode=${1:-rw}
-  [[ "${fs_mode}" == "ro" ]] && mkdir -p /app/ncdiag && chmod 777 /app/ncdiag
-  
+send_crash_dump() {  
   echo "$MARKER_MESSAGE" > /app/ncdiag/sent_crash_dump.log
   cat /app/ncdiag/sent_crash_dump.log
 }
--- tests/send-crash-dump/test.sh
+++ tests/send-crash-dump/test.sh
@@ -11,6 +11,7 @@ source "$SCRIPT_DIR/diag/diag-bootstrap.sh"
 test() {
   container_name=$(random_name "test-run")
 
+  mkdir -p $SCRIPT_DIR/ncdiag && chmod 777 $SCRIPT_DIR/ncdiag
   # shellcheck disable=SC2046
   docker run -d --rm --name "$container_name" \
         $(read_only_params $1) \
@@ -20,7 +21,7 @@ test() {
   docker exec "$container_name" bash -c 'kill -SIGSEGV $(ps ax | grep -v grep | grep java | grep -v bash | awk "{print \$1}")'
   docker logs -f "$container_name" >"$PROC_OUTPUT_FILE"
   docker stop "$container_name" 
-  
+
   <"$PROC_OUTPUT_FILE" grep -Fx -m1 "$MARKER_MESSAGE" >/dev/null || fail "Test error: send_crash_dump function was not called"
   <"$PROC_OUTPUT_FILE" grep "JAVA_TOOL_OPTIONS: -Xmx64m" >/dev/null || fail "Test error: JAVA_TOOL_OPTIONS not handled properly"
 }
