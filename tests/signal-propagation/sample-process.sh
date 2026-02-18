ecfb4810973c27b903112ffe7222f5df83dde72b
Sergey Lisovoy <lis0x90@gmail.com>
2026-02-18 22:18:28 +0500

    chore: test correct non zero exit code handling in entrypoint wait loop

--- tests/signal-propagation/sample-process.sh
+++ tests/signal-propagation/sample-process.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # use SIGUSR1 to avoid sleeping 10 seconds in entrypoint trap handler for SIGTERM
-trap 'echo "Test application: captured SIGUSR1"; exit 0' SIGUSR1
+# Exit with 128+SIGUSR1 (158) to simulate being killed by signal
+trap 'echo "Test application: captured SIGUSR1"; exit 158' SIGUSR1
 
 echo "Waiting for signal"
 while true; do
