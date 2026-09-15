70438add994a450c8760fdd7e306f1cac1ba1484
Sergey Lisovoy <sergey.lisovoy@netcracker.com>
2026-09-15 13:17:56 +0500

    chore: update test

--- tests/java-run/app/Application.java
+++ tests/java-run/app/Application.java
@@ -1,6 +1,6 @@
 public class Application {
     public static void main(String[] args) {
         System.out.println("Started java test process. \nJava Version: " + Runtime.version() +
-                "\nRuntime name: " + System.getProperty("java.runtime.name"));
+                "\nVendor: " + System.getProperty("java.vendor.version"));
     }
 }
